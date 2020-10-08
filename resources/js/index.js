0bae58f2a8c7c5d9e477e8e41998c3de87dd4033
Feven Kitsune <fevenkitsune@gmail.com>
2020-10-08 11:12:00 -0400

    + hee hoo text go brr

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,15 @@
-function hello() {
-    alert("Hello!");
+function helloButton() {
+    document.getElementById("headerText").style.color = getRandomColor();
 }
+
+function getRandomColor() {
+    // https://stackoverflow.com/questions/1484506/random-color-generator
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+document.getElementById("headerText").addEventListener("click", helloButton);
