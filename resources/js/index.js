6c1952899c90cf1a007789405e1783c15686acff
Feven Kitsune <fevenkitsune@gmail.com>
2020-11-12 17:07:10 -0500

    + sacrificed several souls to get this working.

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,15 +1,27 @@
 function helloButton() {
-    document.getElementById("headerText").style.color = getRandomColor();
+  document.getElementById("headerText").style.color = getRandomColor();
 }
 
 function getRandomColor() {
-    // https://stackoverflow.com/questions/1484506/random-color-generator
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  // https://stackoverflow.com/questions/1484506/random-color-generator
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
+
+function getTextColor() {
+  return getComputedStyle(document.documentElement).getPropertyValue("--text-color");
+}
+
+window.addEventListener('load', function () {
+  // nightmare nightmare nightmare
+  var svgGithub = document.querySelector(".svgGithub").contentDocument.getElementsByClassName("svgGithubPath");
+  for (let pathItem of svgGithub) {
+    pathItem.setAttribute("fill", getTextColor());
+  }
+})
 
 document.getElementById("headerText").addEventListener("click", helloButton);
