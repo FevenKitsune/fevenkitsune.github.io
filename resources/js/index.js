d8186f2730f187adcf85b080eae3883a176e3da7
Feven Kitsune <fevenkitsune@gmail.com>
2021-06-18 16:52:11 -0400

    + new defaults for WebStorm + format improvements + removal of depreciated sectors

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,21 +3,21 @@ fevenkitsune.page -> index.js
 */
 
 function init() {
-  document.getElementById("title-head").addEventListener("click", headerColor);
+    document.getElementById("title-head").addEventListener("click", headerColor);
 }
 
 function headerColor() {
-  document.getElementById("title-head").style.color = getRandomColor();
+    document.getElementById("title-head").style.color = getRandomColor();
 }
 
 function getRandomColor() {
-  // https://stackoverflow.com/questions/1484506/random-color-generator
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+    // https://stackoverflow.com/questions/1484506/random-color-generator
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 init();
