09d45d04d2ccc751a2c28f72d6365bc1d1bafaac
Will Stewart <fevenkitsune@gmail.com>
2022-04-22 22:20:31 -0400

    Variable name improvements so it's (slightly) less confusing.

--- tools/resistors/resources/js/resistors.js
+++ tools/resistors/resources/js/resistors.js
@@ -41,7 +41,8 @@ function build_values_from_bands() {
 
 
 // https://www.javascripttutorial.net/javascript-dom/javascript-radio-button/
-const band_options = ['4 Bands', '5 Bands', '6 Bands'];
+const band_count_options = ['4 Bands', '5 Bands', '6 Bands'];
 const band_count_radios = document.querySelector("#band_count_radios");
-band_count_radios.innerHTML = band_options.map((band_option) => `<input type="radio" name="band_count" value="${band_option}" id="${band_option}">
-                                                                <label for="${band_option}">${band_option}</label>`).join(' ');
+band_count_radios.innerHTML = band_count_options.map((count_option) =>
+    `<input type="radio" name="band_count" value="${count_option}" id="${count_option}">
+     <label for="${count_option}">${count_option}</label>`).join(' ');
