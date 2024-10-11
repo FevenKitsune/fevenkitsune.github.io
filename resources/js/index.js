bcea37fec979fab4c0b7d14f062f4b13a2238a69
Will Stewart <fevenkitsune@gmail.com>
2024-10-11 16:18:28 -0400

    cookin now

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,21 +2,3 @@
 fevenkitsune.page -> index.js
 */
 
-var accordion = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-         *  to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "flex") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "flex";
-        }
-    });
-}
